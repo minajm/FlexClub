81777a221649a6c1a7c79d5b5870355b643f46ca
mina jm <iyanim90@gmail.com>
2020-05-25 19:37:52 +0100

    gallary

--- gallery.php
+++ gallery.php
@@ -3,46 +3,27 @@ include_once('header.php');
 ?>
 
     <div class="container">
-        <div class="Features">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                    </div>
-                    <div class="carousel-item">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px"></div>
-                    <div class="carousel-item">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px">
-                        <img class=" " src="image/image1.jpg" alt="First slide" width="367px" height="350px"></div>
+        <div class="card-deck mb-5 mt-4">
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <?php
