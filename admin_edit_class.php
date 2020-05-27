03be22c0b80d953b8b63cacffd9148442916d395
mina jm <iyanim90@gmail.com>
2020-05-27 23:29:36 +0100

    admin edit class (need image fix)

--- admin_edit_class.php
+++ admin_edit_class.php
@@ -1,7 +1,96 @@
 <?php
-include_once('header.php');
-if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
-    ?>
+include ('header.php');
+include "admin_panel_check.php";
+
+$action = null;
+
+if (isset($_GET['new'])) {
+    $action = "new";
+} else if ($_GET['id']) {
+    $action = "edit";
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $title = $_POST['title'];
+    $summery = $_POST['summery'];
+    $photo = "";
+    $link = $_POST['link'];
+
+
+    if ($_FILES["photo"]) {
+        $target_dir = "./image/upload/";
+        $target_file = $target_dir . basename($_FILES["photo"]["name"]);
+
+        $check = getimagesize($_FILES["photo"]["tmp_name"]);
+
+        if ($check !== false) {
+            if (!file_exists($target_file)) {
+                if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+                    $photo = $target_file;
+                }
+            } else {
+                die('photo with the name already exists, please rename and upload again');
+            }
+        } else {
+            die('incorrect image file');
+        }
+    }
+
+    if ($action === 'new') {
+        $connection->query('INSERT INTO class (title, summery, image, link) VALUES ("'. $title .'", "'. $summery .'", "'. $photo .'", "'. $link .'");');
+    } else if ($action === 'edit') {
+        $id = $_GET['id'];
+        $connection->query('UPDATE class SET title="' . $title . '", summery="' . $summery . '", image="' . $photo . '", link="'.$link.'" WHERE id = ' . $id . ';');
+    }
+
+    echo "<script>window.location.replace(\"/FlexClub/admin_edit_class.php\");</script>";
+}
+
+function renderForm($connection, $action, $id=null) {
+    $title = "";
+    $summery = "";
+    $link = "";
+
+    if ($id != null) {
+        $sql = "SELECT * FROM class where id=" . $id;
+        $result = mysqli_query($connection, $sql);
+
+        if (!is_null($result)) {
+            $row = $result->fetch_assoc();
+
+            $title = $row['title'];
+            $summery = $row['summery'];
+            $link = $row['link'];
+
+        } else {
+            die('record does not exist');
+        }
+    }
+
+
+    return "<h1>" . ($action === 'new' ? "New Item" : "Edit") . "</h1>
+
+<form method=\"POST\"  enctype=\"multipart/form-data\">
+  <label for=\"title\">Title:</label><br>
+  <input type=\"text\" id=\"title\" name=\"title\" value='" . $title . "'><br>
+  
+  <label for=\"photo\">Photo:</label><br>
+  <input type=\"file\" name=\"photo\" id=\"photo\"><br>
+  
+  <label for=\"link\">Link:</label><br>
+  <input type=\"text\" id=\"link\" name=\"link\" value='" . $link . "'><br>
+  
+  <label for=\"summery\">Summery:</label><br>
+  <textarea  id=\"summery\" name=\"summery\">" . $summery. "</textarea>
+   
+  <br><br>
+  
+  <input type=\"submit\" value=\"Submit\">
+</form>";
+}
+
+?>
+
     <div class="container-fluid">
         <div class="row">
             <?php
@@ -9,32 +98,43 @@ if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
             ?>
 
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-
-
                 <?php
                 $sql = "SELECT COUNT(*) FROM class";
-                $result = mysqli_query($connection, $sql);    // object or null
+                $result = mysqli_query($connection, $sql);
+
                 if (!is_null($result)) {
                     $row = $result->fetch_assoc();
-                    $class_count = $row['COUNT(*)'];
+                    $features_count = $row['COUNT(*)'];
                 }
                 ?>
-                <h2>Classes<span class="float-right"><?php echo "Count : " . $class_count; ?></span></h2>
+                <h2>Feature Boxes<span class="float-right">
+                        <?php echo "Count : " . $features_count; ?></span>
+                </h2>
+
+                <a href="./admin_edit_class.php?new">Add new feature box</a>
+
+                <br>
+                <?php
+                if ($action != null) {
+                    echo renderForm($connection, $action, $_GET['id']);
+                }
+                ?>
+
+                <br>
+
                 <div class="table-responsive">
                     <table class="table table-striped table-sm">
                         <thead>
                         <tr>
                             <th>#</th>
-                            <th>name</th>
-                            <th>summery</th>
-                            <th>
-
-                            </th>
+                            <th>Title</th>
+                            <th>Summery</th>
+                            <th>Image</th>
+                            <th>Link</th>
+                            <th>Action</th>
                         </tr>
                         </thead>
                         <tbody>
-
-
                         <?php
                         $query = "SELECT * FROM class";
                         $result = mysqli_query($connection, $query);    // object or null
@@ -42,13 +142,16 @@ if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
                         if (!is_null($result)) {
                             if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
-                                    $id = $row["id"];
-                                    echo '<tr>
+                                    echo '
+                                        <tr>
                                             <td>' . $row["id"] . '</td>
-                                            <td>' . $row["name"] . '</td>
+                                            <td>' . $row["title"] . '</td>
                                             <td>' . $row["summery"] . '</td>
-                                            <td>' . '<img width="100" height="100" src="' . $row['photo_link'] . '" >' . '</td>
-                                            <td><a class="btn btn-primary" href="http://localhost/hassanProject/class_edit.php?tid=' . $id . '">Edit</a></td>
+                                            <td> 
+                                            '. '<img width="100" height="100" src="' . $row['image'] . '" >' . '
+                                            </td>
+                                            <td>' . $row["link"] . '</td>
+                                            <td><a href="admin_edit_class.php?id=' . $row["id"] . '">Edit</a></td>
                                         </tr>';
                                 }
                             }
@@ -57,11 +160,10 @@ if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
                         </tbody>
                     </table>
                 </div>
-                <img src="">
+            </main>
         </div>
     </div>
-    <?php
-} else {
-    header('Location: http://localhost/HassanProject/login.php');
-}
+
+
+<?php
 include_once('footer.php');
