c70064d703006122a733b80ac7bb87f53f45c3e3
TheRock786000 <saad.pirzada@gmail.com>
2020-05-28 16:35:24 +0100

    Update class.php

--- class.php
+++ class.php
@@ -1,25 +1,34 @@
 <?php
+
+<!-- this is the head -->
 include_once('header.php');
 ?>
-
+<!-- Below div class will define the conatiner -->
     <div class="container">
+        <!-- this class will define the parameters of tehe row -->
         <div class="row bg-light pb-5 pt-5 mb-3">
+            <!-- below class will define teh column and text should be in center so we wrote text-center -->
             <div class="col text-center">
+                <!-- this heading will define the class which represent teh Classes and the text should be in dark  -->
                 <h2 class="text-dark">Classes</h2>
+                <!-- In this belwo class it  is defined as the secondary text which would show information for the classes which offered in Flex club -->
                 <p class="text-secondary">You can find FLEX CLUB classes here.</p>
             </div>
         </div>
+        <!-- Now from here we come to teh class which explains teh card deck -->
         <div class="card-deck">
 
             <?php
+            <!-- below its a command or quercy which will request the information from class for select -->
             $features = mysqli_fetch_all($connection->query("select * from class;"), MYSQLI_ASSOC);
 
             foreach ($features as $feature) {
                 ?>
-
+                <!-- This class will define the coloumn and its location -->
                 <div class="col-md-4">
-
+                   <!-- The card is defned under this class  -->
                     <div class="card  mb-3">
+                        <!-- Below it will define the image which would y -->
                         <img src=" <?= $feature['image']; ?>" class="card-img-top mh-50 " alt="Flex club Class Photo">
                         <div class="card-body">
                             <h5 class="card-title">
